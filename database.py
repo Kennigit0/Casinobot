ad4be2f2872770d0e2bf8940505d2716f0353c85
Kennigit0 <kenpachizarak0011@gmail.com>
2026-05-17 22:08:10 +0530

    fix get_conn

--- database.py
+++ database.py
@@ -8,6 +8,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_conn():
     if DATABASE_URL:
+        import psycopg2
+        conn = psycopg2.connect(DATABASE_URL, sslmode="require")
         return conn, "pg"
     else:
         import sqlite3
