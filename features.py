3537c8cf93755d2cce306537a02e4e478adb964a
Kennigit0 <kenpachizarak0011@gmail.com>
2026-05-23 20:31:38 +0530

    fix rbet in group - move auto_save handler to last

--- features.py
+++ features.py
@@ -315,7 +315,7 @@ def _auto_save_group(message):
         db.save_group(message.chat.id)
 
 def register_features(bot_instance):
-    bot_instance.register_message_handler(_auto_save_group, func=lambda m: m.chat.type in ("group","supergroup"))
+    
     bot_instance.register_callback_query_handler(
         handle_bank_callbacks,
         func=lambda c: c.data in ['bank_dep','bank_with','bank_int','bank_upg','bank_upg_confirm','lb_chips','lb_xp']
